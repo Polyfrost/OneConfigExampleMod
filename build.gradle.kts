8f6a238f613a3da8ea9bf192294745c2af767581
ev chang <wyvestbusiness@gmail.com>
2024-07-04 01:22:27 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -97,7 +97,7 @@ dependencies {
     // If we are building for legacy forge, includes the launch wrapper with `shade` as we configured earlier, as well as mixin 0.7.11
     if (platform.isLegacyForge) {
         compileOnly("org.spongepowered:mixin:0.7.11-SNAPSHOT")
-        shade("cc.polyfrost:oneconfig-wrapper-launchwrapper:1.0.0-beta+")
+        shade("cc.polyfrost:oneconfig-wrapper-launchwrapper:1.0.0-beta17")
     }
 }
 
